0aa5de4c7477e5d64596200d528d60cb27e0b834
KentErik <kent.erik.hagman@gmail.com>
2016-08-05 23:05:35 -0700

    unity project setup
    setting up our unity project

---
+++ BambiUnityProject/Assets/Scripts/BasicScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Hello!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
